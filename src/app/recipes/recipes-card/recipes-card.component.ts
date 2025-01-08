62a7ad95574719d03396c2fe57343a675c46acb8
bena-sudo <joanbenaventcastells@gmail.com>
2025-01-08 16:41:30 +0100

    Add detail route.

--- src/app/recipes/recipes-card/recipes-card.component.ts
+++ src/app/recipes/recipes-card/recipes-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { IRecipe } from '../../interface/i-recipe';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-recipes-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './recipes-card.component.html',
   styleUrl: './recipes-card.component.css',
 })
--- src/app/recipes/recipes-detail/recipes-detail.component.ts
+++ src/app/recipes/recipes-detail/recipes-detail.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recipes-detail',
   imports: [],
   templateUrl: './recipes-detail.component.html',
-  styleUrl: './recipes-detail.component.css'
+  styleUrl: './recipes-detail.component.css',
 })
 export class RecipesDetailComponent {
-
+  @Input('id') recipeID?: string;
 }
