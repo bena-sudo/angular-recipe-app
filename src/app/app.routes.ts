48d7f8d8b849fc6240963b50482c79c97a682d47
bena-sudo <joanbenaventcastells@gmail.com>
2025-01-08 15:54:09 +0100

    Add home page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MainComponent } from './components/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'main', component: MainComponent},
+    {path: '**', pathMatch: 'full', redirectTo: ''}
+];
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RecipesListComponent } from '../../recipes/recipes-list/recipes-list.component';
 import { RecipesTableComponent } from "../../recipes/recipes-table/recipes-table.component";
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-main',
-  imports: [RecipesListComponent, RecipesTableComponent],
+  imports: [RecipesListComponent, RecipesTableComponent, HeaderComponent, FooterComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
