2e07a4f1a39082921c648b19182e3a145a18a94b
bena-sudo <joanbenaventcastells@gmail.com>
2025-01-08 16:13:50 +0100

    Add route and datail paige.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { MainComponent } from './components/main/main.component';
+import { RecipesTableComponent } from './recipes/recipes-table/recipes-table.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
     {path: 'main', component: MainComponent},
-    {path: '**', pathMatch: 'full', redirectTo: ''}
+    {path: 'list', component: RecipesListComponent},
+    {path: 'table', component: RecipesTableComponent},
+    {path: 'recipe/:id', component: RecipesDetailComponent},
+    {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
