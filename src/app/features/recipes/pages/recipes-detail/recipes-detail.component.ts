9bd9bf1dedbfbb9b9c7cdac87143d6bdebca47d7
bena-sudo <joanbenaventcastells@gmail.com>
2025-01-09 23:30:15 +0100

    Create detail view.

--- src/app/features/recipes/pages/recipes-detail/recipes-detail.component.ts
+++ src/app/features/recipes/pages/recipes-detail/recipes-detail.component.ts
@@ -1,11 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FooterComponent } from "../../../../shared/components/footer/footer.component";
+import { HeaderComponent } from "../../../../shared/components/header/header.component";
 
 @Component({
   selector: 'app-recipes-detail',
-  imports: [],
+  imports: [FooterComponent, HeaderComponent],
   templateUrl: './recipes-detail.component.html',
   styleUrl: './recipes-detail.component.css',
 })
-export class RecipesDetailComponent {
+export class RecipesDetailComponent implements OnInit {
   @Input('id') recipeID?: string;
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
