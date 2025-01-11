de93ba991ba04b6755471ce26cedfe6264c2af7f
bena-sudo <joanbenaventcastells@gmail.com>
2025-01-11 01:06:44 +0100

    Show recipe content.

--- src/app/features/recipes/pages/recipe-detail/recipe-detail.component.ts
+++ src/app/features/recipes/pages/recipe-detail/recipe-detail.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { Recipe } from '../../../../core/models/recipe';
 import { RecipeService } from '../../service/recipes.service';
 import { HeaderComponent } from '../../../../shared/components/header/header.component';
-import { FooterComponent } from "../../../../shared/components/footer/footer.component";
+import { FooterComponent } from '../../../../shared/components/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-detail',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.css',
 })
@@ -28,4 +29,24 @@ export class RecipeDetailComponent {
       console.error('Error when loading recipe:', error);
     }
   }
+
+  getIngredients(): { name: string; quantity: string }[] {
+    if (!this.recipe) return [];
+    const ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+      const ingredient = (this.recipe as any)[`strIngredient${i}`];
+      const measure = (this.recipe as any)[`strMeasure${i}`];
+      if (ingredient && ingredient.trim()) {
+        ingredients.push({ name: ingredient, quantity: measure || '' });
+      }
+    }
+    return ingredients;
+  }
+  
+  getInstructionParagraphs(): string[] {
+    if (!this.recipe?.strInstructions) return [];
+    return this.recipe.strInstructions.split(/(?:\n)+/).filter(instruction => instruction.trim());
+  }
+  
+
 }
