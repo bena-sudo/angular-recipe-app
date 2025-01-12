8c00bd741a526d920eaee7986c2234a5755fa04c
bena-sudo <joanbenaventcastells@gmail.com>
2025-01-12 23:27:57 +0100

    Add page to create recipe.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { LoginComponent } from './features/auth/pages/login/login.component';
 import { RecipeDetailComponent } from './features/recipes/pages/recipe-detail/recipe-detail.component';
 import { AboutComponent } from './shared/components/about/about.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { RecipeCreateComponent } from './features/recipes/pages/recipe-create/recipe-create.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: RecipeHomeComponent },
   { path: 'main', component: RecipeMainComponent },
   { path: 'recipe/:id', component: RecipeDetailComponent },
+  { path: 'create', component: RecipeCreateComponent },
   { path: 'login', component: LoginComponent },
   { path: 'about', component: AboutComponent },
   { path: '**', component: NotFoundComponent },
