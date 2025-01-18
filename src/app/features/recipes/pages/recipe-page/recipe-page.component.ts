6ab51455c9012ba88c4d18dd0c640c89b5602cc5
bena-sudo <joanbenaventcastells@gmail.com>
2025-01-18 16:33:03 +0100

    Remove components.

--- src/app/features/recipes/pages/recipe-page/recipe-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-recipe-page',
-  imports: [],
-  templateUrl: './recipe-page.component.html',
-  styleUrl: './recipe-page.component.css',
-})
-export class RecipePageComponent {}
